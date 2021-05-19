4619cfc1b493f02f20ba7953a72608f29d2d4602
Brendon Conatser <b.conatser21@gmail.com>
2021-05-19 13:45:29 -0500

    trying to figure out the count

--- src/main/kotlin/com/lifeway/codechallenge/CodechallengeApplication.kt
+++ src/main/kotlin/com/lifeway/codechallenge/CodechallengeApplication.kt
@@ -5,21 +5,42 @@ import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+
 @SpringBootApplication
 class CodechallengeApplication
+
 
 fun main(args: Array<String>) {
 	runApplication<CodechallengeApplication>(*args)
 }
 
+
 @RestController
 class MessageResource {
+
+
+
+
 	@GetMapping
-	fun index(): List<Message> = listOf(
+
+	fun wordCount(Message: String): Int {
+		val strArray = Message.split(" ".toRegex()).toTypedArray()
+		var count = 0
+
+		for (s in strArray) {
+			if (s != "") {
+				count++
+			}
+		}
+		return count
+	}
+
+	fun index(): List<Any> = listOf(
 			Message("1", "The dog ran."),
 			Message("2", "How many words are in me?"),
 			Message("3", "My wife and I will celebrate ten years this year!"),
 	)
+
 }
 
 data class Message(val id: String?, val text: String)
