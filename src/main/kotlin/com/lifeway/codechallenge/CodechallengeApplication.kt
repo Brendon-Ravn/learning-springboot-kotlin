04f7b3ba0aed52a2c35d7f618ced0253c7d389ec
Brendon Conatser <b.conatser21@gmail.com>
2021-05-24 00:07:52 -0500

    Started adding db capabilities

--- src/main/kotlin/com/lifeway/codechallenge/CodechallengeApplication.kt
+++ src/main/kotlin/com/lifeway/codechallenge/CodechallengeApplication.kt
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.PostMapping
-
-
+import org.springframework.stereotype.Service
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
-
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
 
@@ -30,15 +28,7 @@ class MessageResource(val service: MessageService) {
 
 
 	@GetMapping
-
-
-
-	fun index(): List<Any> = listOf(
-			Message("1", "The dog ran."),
-			Message("2", "How many words are in me?"),
-			Message("3", "My wife and I will celebrate ten years this year!"),
-
-	)
+	fun index(): List<Message> = service.findMessages()
 
 }
 
